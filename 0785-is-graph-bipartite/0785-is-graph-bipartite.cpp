dbd090726e02f3a46ccf517e87960c5ba34f8ae1
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-08-08 23:03:38 +0530

    Time: 0 ms (100%), Space: 17.6 MB (38.13%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -21,6 +21,34 @@ public:
         }
         return true;
     }
+    bool bfs(vector<vector<int>>& adj,int currcolor,vector<int>& color,int u)
+    {
+        queue<pair<int,int>>q;
+        color[u]=currcolor;
+        q.push({u,currcolor});
+
+        while(!q.empty())
+        {
+            auto p=q.front();
+            int v=p.first;
+            int ColorOfV=p.second;
+            q.pop();
+            for(int& k:adj[v])
+            {
+                if(color[k]==ColorOfV)
+                {
+                    return false;
+                }
+                if(color[k]==-1)
+                {
+                    int newcolor=1-ColorOfV;
+                    color[k]=newcolor;
+                    q.push({k,newcolor});
+                }
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>color(n,-1);
@@ -28,7 +56,7 @@ public:
         {
             if(color[i]==-1)
             {
-                if(dfs(graph,1,color,i)==false)
+                if(bfs(graph,1,color,i)==false)
                   return false;
             }
         }
