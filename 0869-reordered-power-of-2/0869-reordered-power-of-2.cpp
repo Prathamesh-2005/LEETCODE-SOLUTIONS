c9291c2c2be9389ab9c217b53f1f24c8b752d602
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-08-11 14:25:31 +0530

    Time: 0 ms (100%), Space: 9.1 MB (50.15%) - LeetHub

---
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+      string s=to_string(n);
+      sort(s.begin(),s.end());
+
+      for(int i=0;i<31;i++)
+      {
+         int num=(1<<i);
+         string str=to_string(num);
+         sort(str.begin(),str.end());
+         if(str==s)
+         {
+            return true;
+         }
+      }
+    return false;
+    }
+};
