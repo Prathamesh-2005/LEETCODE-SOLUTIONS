ed085473559d70f88c1f316c39c9f407d7e21fbe
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-09-16 18:48:08 +0530

    Time: 68 ms (6.02%), Space: 133.2 MB (18.52%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    int gcd(int a ,int b)
-    {
-        while(b!=0)
-        {
-            int temp=b;
-            b=a%b;
-            a=temp;
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
         return a;
     }
-    int lcm(int a,int b)
-    {
-        return (a/gcd(a,b))*b;
-    }
-    vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> ans;
-        
-        for (int x : nums) {
-            ans.push_back(x);
 
-            while (ans.size() >= 2) {
-                int a = ans.back(); ans.pop_back();
-                int b = ans.back(); ans.pop_back();
+    int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
+
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        stack<int> st;
+
+        for (int num : nums) {
+            st.push(num);
+            while (st.size() > 1) {
+                int a = st.top(); st.pop();
+                int b = st.top(); st.pop();
 
                 int g = gcd(a, b);
-                if (g > 1) {
-                    ans.push_back(lcm(a, b)); 
-                } else {
-                    ans.push_back(b);
-                    ans.push_back(a);
+
+                if (g == 1) {
+                    st.push(b);
+                    st.push(a);
                     break;
+                } else {
+                    st.push(lcm(a, b));
                 }
             }
+        }
+
+        vector<int> ans(st.size());
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
         }
 
         return ans;
