0f183ee8a00c4df17649419c8c15864a25041afa
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-10-20 14:06:49 +0530

    Time: 3 ms (1.17%), Space: 43.2 MB (8.35%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        return Arrays.stream(operations)
+        .mapToInt(x->x.contains("+")?1:-1)
+        .sum();
+    }
+}
