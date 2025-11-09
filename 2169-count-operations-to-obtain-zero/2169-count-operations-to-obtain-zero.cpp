cb83c98c19a9d89d8997bf42cfc9a5304d49c0c8
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-11-09 16:42:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.15%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,22 @@
+#define ll long long 
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        ll ans=0;
+
+        while(num1!=0 && num2!=0)
+        {
+            if(num1>=num2)
+            {
+                num1=num1-num2;
+                ans++;
+            }
+            else{
+                num2=num2-num1;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
