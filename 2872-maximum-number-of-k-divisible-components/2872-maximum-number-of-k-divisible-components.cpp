80009977b35646cc1f4b696e2793ef5fafc316ef
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2025-11-28 18:49:49 +0530

    Time: 159 ms (36.3%), Space: 195 MB (13.17%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+void dfs(int node, int parent, vector<int>& values,
+         unordered_map<int,vector<int>>& mp, int& ans, int k, long long& sum)
+{
+    sum = values[node];
+
+    for(int child : mp[node]) {
+        if(child != parent) {
+            long long childSum = 0;
+            dfs(child, node, values, mp, ans, k, childSum);
+            sum += childSum;
+        }
+    }
+
+    if(sum % k == 0) ans++; 
+}
+
+int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+    unordered_map<int,vector<int>> mp;
+    for(auto &e : edges){
+        mp[e[0]].push_back(e[1]);
+        mp[e[1]].push_back(e[0]);
+    }
+
+    int ans = 0;
+    long long total = 0;
+    dfs(0, -1, values, mp, ans, k, total);
+    return ans;
+}
+
+};
