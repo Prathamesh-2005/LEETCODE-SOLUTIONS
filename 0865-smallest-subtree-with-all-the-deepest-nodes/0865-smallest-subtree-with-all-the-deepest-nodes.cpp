2b67166e7b3de67b5da8dc36276cc68a976b1dba
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-01-09 16:53:25 +0530

    Time: 0 ms (100%), Space: 16.4 MB (27.77%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,97 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int mxd=0;
+    void find(TreeNode* root,int depth,vector<TreeNode*>& vec)
+    {
+        if(!root)
+        {
+            return ;
+        }
+
+        if(!root->left && !root->right)
+        {
+            if(depth>mxd)
+            {
+                mxd=depth;
+                vec.clear();
+                vec.push_back(root);
+            }
+            else if(depth==mxd)
+            {
+                vec.push_back(root);
+            }
+        }
+        find(root->left,depth+1,vec);
+        find(root->right,depth+1,vec);
+    }
+
+    TreeNode* lca(TreeNode* root,TreeNode* p,TreeNode* q)
+    {
+        if(!root){
+            return root;
+        }
+
+        if(root==p || root==q)
+        {
+            return root;
+        }
+        TreeNode* left=lca(root->left,p,q);
+        TreeNode* right=lca(root->right,p,q);
+
+        if(left!=nullptr && right!=nullptr)
+        {
+            return root;
+        }
+
+        if(left!=nullptr)
+        {
+            return left;
+        }
+
+        return right;
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if(root==nullptr)
+        {
+            return root;
+        }
+        TreeNode* left=nullptr;
+        TreeNode* right=nullptr;
+
+        vector<TreeNode*>vec;
+
+        find(root,1,vec);
+        if(vec.size()==2)
+        {
+            left=vec[0];
+            right=vec[1];
+        }
+        else if(vec.size()==1)
+        {
+            return vec[0];
+        }
+        else if(vec.size()>2)
+        {
+            TreeNode* lc=vec[0];
+            for(int i=1;i<vec.size();i++)
+            {
+                lc=lca(root,lc,vec[i]);
+            }
+            return lc;
+        }
+        TreeNode* node=lca(root,left,right);
+
+        return node;
+    }
+};
