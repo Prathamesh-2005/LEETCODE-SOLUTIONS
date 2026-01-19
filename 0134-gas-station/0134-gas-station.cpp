234052250850f71808468d6d5af9dc894a9f2a3e
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-01-19 18:32:52 +0530

    Time: 2 ms (39.98%), Space: 112.4 MB (85.14%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    //     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    //     int n = gas.size();
+
+    //     for (int i = 0; i < n; i++) {
+    //         int curr = 0;
+    //         bool ok = true;
+
+    //         for (int j = 0; j < n; j++) {
+    //             int k = (i + j) % n;   
+    //             curr += gas[k];
+
+    //             if (curr < cost[k]) {
+    //                 ok = false;
+    //                 break;
+    //             }
+    //             curr -= cost[k];
+    //         }
+
+    //         if (ok) return i;
+    //     }
+    //     return -1;
+    // }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
         int sum1=accumulate(gas.begin(),gas.end(),0);
