129679b72146ea3efe1b0a948d1ea2da6dc9fab6
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-01-30 18:12:56 +0530

    Time: 2636 ms (5%), Space: 16.2 MB (6.28%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(string s,string p)
+    {
+        if(p.length()==0)
+        {
+            if(s.length()==0)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool first=(!s.empty() && (p[0]==s[0] || p[0]=='.'));
+        if(p[1]=='*')
+        {
+            bool not_take=solve(s,p.substr(2));
+            bool take=(first && solve(s.substr(1),p));
+            return (take || not_take);
+        }else{
+            return (first && solve(s.substr(1),p.substr(1)));
+        }
+    }
+    bool isMatch(string s, string p) {
+        return solve(s,p);
+    }
+};
