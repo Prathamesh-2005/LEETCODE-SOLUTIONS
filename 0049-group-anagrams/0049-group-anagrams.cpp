e40caa16196f96bb2181215185d4150478efc453
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-06 15:42:15 +0530

    Time: 23 ms (32.63%), Space: 29.7 MB (20.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    string solve(string& str)
+    {
+        int n=str.length();
+        vector<int>arr(26,0);
+
+        for(int i=0;i<n;i++)
+        {
+            arr[str[i]-'a']++;
+        }
+        string ans="";
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]>0)
+                ans+=string(arr[i],i+'a');
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
           vector<vector<string>>ans;
           unordered_map<string,vector<string>>mp;
+          int n=strs.size();
 
-          for(auto word:strs)
+          for(int i=0;i<n;i++)
           {
-            string str=word;
-            sort(str.begin(),str.end());
-            mp[str].push_back(word);
+             string word=strs[i];
+             string str=solve(word);
+             mp[str].push_back(word);
           }
-
           for(auto& m:mp)
           {
             ans.push_back(m.second);
