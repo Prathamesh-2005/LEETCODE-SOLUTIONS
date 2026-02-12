7607efb17571c27e376f52a9d8865fcadea4fee9
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-12 09:21:36 +0530

    Time: 204 ms (55.39%), Space: 41.4 MB (23.27%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     bool check(unordered_map<char,int>& mp)
     {
-        int cnt=mp.begin()->second;;
+        int freq=mp.begin()->second;
+
         for(auto& m:mp)
         {
-            if(m.second!=cnt)
+            if(m.second!=freq)
             {
                 return false;
             }
         }
-
         return true;
     }
     int longestBalanced(string s) {
         int n=s.length();
         int ans=0;
+
         for(int i=0;i<n;i++)
         {
             unordered_map<char,int>mp;
@@ -28,7 +29,6 @@ public:
                 }
             }
         }
-
-        return ans ;
+        return ans;
     }
 };
