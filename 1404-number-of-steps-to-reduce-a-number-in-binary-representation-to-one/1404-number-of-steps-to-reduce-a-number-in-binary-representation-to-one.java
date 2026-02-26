1fb7a2867d1a80e300ceb6caa5b685ffe45ca864
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-26 12:04:43 +0530

    Time: 17 ms (5.01%), Space: 46.2 MB (12.98%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,29 +1,51 @@
 class Solution {
-    public int generate(String s)
+    // public int generate(String s)
+    // {
+    //     int num=0;
+    //     int n=s.length();
+    //     int pow=0;
+    //     for(int i=n-1;i>=0;i--)
+    //     {
+    //         int c=s.charAt(i)-'0';
+    //         num+=(c<<pow);
+    //         pow++;
+    //     }
+    //     System.out.print(num);
+    //     return num;
+    // }
+    public void add(StringBuilder sb)
     {
-        int num=0;
-        int n=s.length();
-        int pow=0;
-        for(int i=n-1;i>=0;i--)
+        int n=sb.length();
+        StringBuilder ans=new StringBuilder();
+        StringBuilder one=new StringBuilder("1");
+        int carry=0;
+        int i=n-1,j=one.length()-1,sum=0;
+        while(i>=0 || j>=0 || carry!=0)
         {
-            int c=s.charAt(i)-'0';
-            num+=(c<<pow);
-            pow++;
+            int num1=(i<0 ? 0 : sb.charAt(i)-'0'); 
+            int num2=(j<0 ? 0 : one.charAt(j)-'0'); 
+            int curr=num1+num2+carry;
+            sum=(curr%2);
+            carry=curr/2;
+            ans.append(sum);
+            i--;
+            j--;
         }
-        System.out.print(num);
-        return num;
+        ans.reverse();
+        sb.setLength(0);
+        sb.append(ans);
     }
     public int numSteps(String s) {
         int n=s.length();
         int ans=0;
-        int num=generate(s);
-        while(num!=1)
+        StringBuilder sb=new StringBuilder(s);
+        while(!sb.toString().equals("1"))
         {
-            if((num & 1)==1)
+            if(sb.charAt(sb.length()-1)=='0')
             {
-                num+=1;
+                sb.deleteCharAt(sb.length()-1);
             }else{
-                num/=2;
+                add(sb);
             }
             ans++;
         }
