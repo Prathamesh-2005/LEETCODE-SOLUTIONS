522aab5eb15504f06c73f58bb4efaac3591fad83
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-02-28 12:37:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    typedef long long ll;
+    string solve(int num)
+    {
+        string ans="";
+        while(num!=0)
+        {
+            ans+=((num%2)+'0');
+            num/=2;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    int concatenatedBinary(int n) {
+        int mod=1e9+7;
+        ll ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            string curr=solve(i);
+            int n=curr.length();
+            ans=(ans<<n)+i;
+        }
+        return ans%mod;
+    }
+};
