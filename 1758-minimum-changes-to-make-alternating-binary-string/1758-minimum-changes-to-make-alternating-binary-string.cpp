4a141271ab0dea355685f1993e27d36be8a1cbfd
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-03-05 12:41:41 +0530

    Time: 4 ms (4.15%), Space: 11.3 MB (6.11%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,15 +2,42 @@ class Solution {
 public:
     int minOperations(string s) {
         int n=s.length();
-        int ans=0;
-        for(int i=1;i<n;i++)
+        int ans1=0;
+        vector<int>vec1(n);
+        vector<int>vec2(n);
+
+        for(int i=0;i<n;i++)
         {
-            if(s[i]==s[i-1])
+            if(i & 1)
             {
-                ans++;
-                s[i]= (s[i-1]=='1' ? '0' :'1');
+                vec1[i]=1;
+            }else{
+                vec1[i]=0;
             }
         }
-        return ans;
+        for(int i=0;i<n;i++)
+        {
+            if(i & 1)
+            {
+                vec2[i]=0;
+            }else{
+                vec2[i]=1;
+            }
+        }
+
+        int ans2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(vec1[i]!=s[i]-'0')
+            {
+                ans1++;
+            }
+            if(vec2[i]!=s[i]-'0')
+            {
+                ans2++;
+            }
+        }
+        
+        return min(ans1,ans2);
     }
 };
