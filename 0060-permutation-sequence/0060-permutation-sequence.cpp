f2882746f06dfd9483611ef1c036e86b8a654e98
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-04-03 16:01:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string solve(string& curr,int n,int& k,vector<bool>& visit,string& ans)
+    {
+        if(curr.size()==n)
+        {
+            k=k-1;
+            if(k==0)
+            {
+                ans=curr;
+            }
+            cout<<curr;
+            return "";
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(visit[i])
+            {
+                continue;
+            }
+            visit[i]=true;
+            curr.push_back(i+'0');
+            solve(curr,n,k,visit,ans);
+            curr.pop_back();
+            visit[i]=false;
+        }
+        return "";
+    }
+    string getPermutation(int n, int k) {
+        vector<bool>visit(n+1,false);
+        string curr="";
+        string ans="";
+        solve(curr,n,k,visit,ans);
+        return ans;
+    }
+};
