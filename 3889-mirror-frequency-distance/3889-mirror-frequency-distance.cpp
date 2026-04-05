7d1a9dad700b229d9c4b39a4317a3fc2597f01b6
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-04-05 14:57:47 +0530

    Time: 347 ms (5.02%), Space: 90.2 MB (5.13%) - LeetHub

---
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int mirrorFrequency(string s) {
+        unordered_map<char,int>mp;
+
+        for(auto& c:s)
+        {
+            mp[c]++;
+        }
+
+        string miro="";
+
+        for(auto& c: s)
+        {
+            if(isalpha(tolower(c)))
+            {
+                char m='a'+('z'-c);
+                miro+=m;
+            }else{
+                int dig=9-(c-'0');
+                char m=dig+'0';
+                miro+=m;
+            }
+        }
+        // cout<<miro<<endl;
+
+        int ans=0;
+        unordered_set<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s[i];
+            if(st.find(ch)!=st.end())
+            {
+                continue;
+            }
+            ans+=(abs(mp[ch]-mp[miro[i]]));
+            st.insert(ch);
+            st.insert(miro[i]);
+        }
+        return ans;
+    }
+};
