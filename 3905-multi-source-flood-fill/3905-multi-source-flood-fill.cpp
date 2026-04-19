62d8ede846504f452de6d40be951e8a536a1abf5
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-04-19 14:45:20 +0530

    Time: 421 ms (35.26%), Space: 273.5 MB (39.37%) - LeetHub

--- 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
+++ 3905-multi-source-flood-fill/3905-multi-source-flood-fill.cpp
@@ -1,54 +1,73 @@
 class Solution {
 public:
-    vector<vector<int>>direc={{-1,0},{0,-1},{1,0},{0,1}};
+    vector<vector<int>> direc = {{-1,0},{0,-1},{1,0},{0,1}};
     bool safe(int i,int j,int n,int m)
     {
         return (i>=0 && j>=0 && i<n && j<m);
     }
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
-        vector<vector<int>>ans;
-        vector<vector<int>>grid(n,vector<int>(m,0));
 
-        queue<pair<int,int>>q;
+        vector<vector<int>> ans;
 
-        
-        for(int i=0;i<sources.size();i++)
+        vector<vector<int>> grid(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
+
+        for(auto& vec : sources)
         {
-            for(int j=0;j<sources[0].size();j++)
-            {
-                q.push({i,j});
-                grid[i][j]=sources[i][j];
-            }
+            int i = vec[0];
+            int j = vec[1];
+            int col = vec[2];
+
+            grid[i][j] = max(grid[i][j], col);
         }
+
+        for(auto& vec : sources)
+        {
+            int i = vec[0];
+            int j = vec[1];
+
+            if(grid[i][j] == vec[2])
+                q.push({i,j});
+        }
+
         while(!q.empty())
         {
-            int k=q.size();
+            int k = q.size();
+
+            map<pair<int,int>,int> mp;  
 
             while(k--)
             {
-                auto p=q.front();
+                auto p = q.front();
                 q.pop();
-                int i_=p.first;
-                int j_=p.second;
-                int col=grid[i_][j_];
 
-                for(auto& dir:direc)
+                int i_ = p.first;
+                int j_ = p.second;
+                int col = grid[i_][j_];
+
+                for(auto& dir : direc)
                 {
-                    int I=i_+dir[0];
-                    int J=j_+dir[1];
+                    int I = i_+dir[0];
+                    int J = j_+dir[1];
 
-                    if(safe(I,J,n,m))
+                    if(safe(I,J,n,m) && grid[I][J] == 0)
                     {
-                        if(grid[I][J]==1 && col==2)
-                        {
-                            grid[I][J]=2;
-                        }
-                        grid[I][J]=col;
+                        mp[{I,J}] = max(mp[{I,J}], col);
                     }
-                    q.push({I,J});
                 }
             }
+
+            for(auto &it : mp)
+            {
+                int I = it.first.first;
+                int J = it.first.second;
+                int col = it.second;
+
+                grid[I][J] = col;
+                q.push({I,J});
+            }
         }
+
         return grid;
     }
 };
