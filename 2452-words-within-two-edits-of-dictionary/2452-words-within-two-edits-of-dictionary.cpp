45ae7793e867d633856f3b3c129d046d0373327e
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-04-22 18:27:50 +0530

    Time: 7 ms (64.17%), Space: 34.4 MB (11.23%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,49 +1,89 @@
+// class Trie{
+//     public:
+//     struct trienode{
+//         bool end;
+//         trie* child[26];
+//     };
+//     trienode* get()
+//     {
+//         trienode* node=new trienode();
+//         node->end=false;
+//         for(int i=0;i<26;i++)
+//         {
+//             node->child[i]=nullptr;
+//         }
+
+//         return node;
+//     } 
+
+// }
 class Solution {
 public:
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        vector<string>ans;
-        unordered_map<string,int>mp;
-        int n=queries.size();
-        int m=dictionary.size();
+    struct trienode {
+        bool end;
+        trienode* child[26];
+    };
 
-        for(int i=0;i<n;i++)
-        {
-            string curr=queries[i];
-            for(int j=0;j<m;j++)
-            {
-                string dict=dictionary[j];
-                if(curr==dict)
-                {
-                    if(mp.count(curr)==0)
-                    {
-                        ans.push_back(curr);
-                        mp[curr]++;
-                    }
-                }else{
-                    if(dict.length()!=curr.length())
-                    {
-                        continue;
-                    }
-                    int k=curr.length();
-                    int cnt=0;
-                    for(int p=0;p<k;p++)
-                    {
-                        if(curr[p]!=dict[p])
-                        {
-                            cnt++;
-                        }
-                    }
-                    if(cnt<=2)
-                    {
-                        if(mp.count(curr)==0)
-                        {
-                            ans.push_back(curr);
-                            mp[curr]++;
-                        }
-                    }
+    trienode* get() {
+        trienode* node = new trienode();
+        node->end = false;
+        for (int i = 0; i < 26; i++) {
+            node->child[i] = nullptr;
+        }
+        return node;
+    }
+
+    trienode* root;
+
+    Solution() {
+        root = get();
+    }
+
+    void insert(string& word) {
+        trienode* crawl = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (crawl->child[idx] == nullptr) {
+                crawl->child[idx] = get();
+            }
+            crawl = crawl->child[idx];
+        }
+        crawl->end = true;
+    }
+
+    bool dfs(trienode* node, string& word, int pos, int cnt) {
+        if (cnt > 2) return false;
+
+        if (pos == word.size()) {
+            return node->end;
+        }
+
+        int idx = word[pos] - 'a';
+
+        for (int i = 0; i < 26; i++) {
+            if (node->child[i] != nullptr) {
+                if (dfs(node->child[i], word, pos + 1, cnt + (i != idx))) {
+                    return true;
                 }
             }
         }
+
+        return false;
+    }
+
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        for (auto &word : dictionary) {
+            insert(word);
+        }
+
+        vector<string> ans;
+
+        for (auto &q : queries) {
+            if (dfs(root, q, 0, 0)) {
+                ans.push_back(q);
+            }
+        }
+
         return ans;
     }
 };
