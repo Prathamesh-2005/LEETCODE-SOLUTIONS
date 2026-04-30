e7dabbca845687d69e8dadbf7ada72d329e91ffb
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-04-30 09:51:30 +0530

    Time: 849 ms (56.62%), Space: 499.5 MB (28.75%) - LeetHub

--- 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
+++ 3742-maximum-path-score-in-a-grid/3742-maximum-path-score-in-a-grid.cpp
@@ -50,7 +50,7 @@ public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(2,-1)));
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(k+1,-1)));
         int ans=solve(0,0,grid,k,dp);
         return (ans==INT_MIN ? -1 : ans);
     }
