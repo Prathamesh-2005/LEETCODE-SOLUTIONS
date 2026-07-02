5bf41fce15b7270c2e2b74cfc89cac439a9559f1
Prathamesh Sahebrav  Jadhav <159058210+Prathamesh-2005@users.noreply.github.com>
2026-07-02 09:55:19 +0530

    Time: 19 ms (60.56%), Space: 30.6 MB (75.65%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> direc = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    bool safe(int i,int j,int n,int m)
+    {
+        return i>=0 && i<n && j>=0 && j<m;
+    }
+
+    bool solve(int i,int j,vector<vector<int>>& grid,int health,
+               int n,int m,vector<vector<int>>& vis)
+    {
+        if(!safe(i,j,n,m))
+            return false;
+
+        health -= grid[i][j];
+
+        if(health < 1)
+            return false;
+
+        if(i==n-1 && j==m-1)
+            return true;
+
+        if(vis[i][j] >= health)
+            return false;
+
+        vis[i][j] = health;
+
+        for(auto &d : direc)
+        {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            if(solve(ni,nj,grid,health,n,m,vis))
+                return true;
+        }
+
+        return false;
+    }
+
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+
+        return solve(0,0,grid,health,n,m,vis);
+    }
+};
